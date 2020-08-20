44b4074a7e8e89771ed231b007ee6629aecdfcb4
luiscavazos <65502416+luiscavazos@users.noreply.github.com>
2020-08-20 17:05:46 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-# OpenWeatherMap API Key
-weather_api_key = "a34154200c5d4c099a9e9ed04a7d4e1e"
-weather_api_key_2 = "a34154200c5d4c099a9e9ed04a7d4e1e"
-# Google API Key
-g_key = "AIzaSyBEsOrhKjPG0JjTUHHhbbtWFjCNS_UQ6Nw"
+#Password
